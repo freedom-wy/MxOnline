332fcbf07db0fd97e6bd4d8be65c42462cf077c1
freedom-wy <dazhuang_python@sina.com>
2022-02-16 15:23:01 +0800

    用户操作相关表设计

--- MxOnlineProject/apps/operations/models.py
+++ MxOnlineProject/apps/operations/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from utils.models_base import BaseModels
+from apps.courses.models import Course
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+UserProfile = get_user_model()
+
+
+class UserAsk(BaseModels):
+    """
+    用户咨询
+    """
+    name = models.CharField('姓名', max_length=20)
+    mobile = models.CharField('手机', max_length=11)
+    course_name = models.CharField('课程名', max_length=50)
+
+    class Meta:
+        verbose_name = '用户咨询'
+        verbose_name_plural = verbose_name
+
+
+class CourseComments(BaseModels):
+    """
+    课程评论
+    """
+    user = models.ForeignKey(UserProfile, verbose_name='用户', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, verbose_name='课程', on_delete=models.CASCADE)
+    comments = models.CharField('评论', max_length=200)
+
+    class Meta:
+        verbose_name = '课程评论'
+        verbose_name_plural = verbose_name
+
+
+class UserFavorite(BaseModels):
+    """
+    用户收藏
+    """
+    FAV_TYPE = (
+        (1, '课程'),
+        (2, '课程机构'),
+        (3, '讲师')
+    )
+
+    user = models.ForeignKey(UserProfile, verbose_name='用户', on_delete=models.CASCADE)
+    fav_id = models.IntegerField('数据id', default=0)
+    fav_type = models.IntegerField(verbose_name='收藏类型', choices=FAV_TYPE, default=1)
+
+    class Meta:
+        verbose_name = '用户收藏'
+        verbose_name_plural = verbose_name
+
+
+class UserMessage(BaseModels):
+    """
+    消息通知
+    """
+    user = models.IntegerField('接受用户', default=0)
+    message = models.CharField('消息内容', max_length=500)
+    has_read = models.BooleanField('是否已读', default=False)
+
+    class Meta:
+        verbose_name = '用户消息'
+        verbose_name_plural = verbose_name
+
+
+class UserCourse(BaseModels):
+    """
+    用户学习的课程
+    """
+    user = models.ForeignKey(UserProfile, verbose_name='用户', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, verbose_name='课程', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '用户课程'
+        verbose_name_plural = verbose_name
