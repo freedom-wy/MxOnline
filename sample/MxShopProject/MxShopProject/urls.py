6b982a8289aba11b42e956cd0017bd4ead5139cb
freedom-wy <dazhuang_python@sina.com>
2022-02-21 14:37:43 +0800

    通过ModelSerializer直接转换模型类中的数据

--- sample/MxShopProject/MxShopProject/urls.py
+++ sample/MxShopProject/MxShopProject/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # 通过django的serializers对模型类数据进行序列化并向前端返回数据
 from apps.goods.views import GoodsListViewSource
 from apps.goods.views import GoodsListViewDemo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 仅是在api接口页面上显示一个登录按钮?
+    path('api-auth/', include('rest_framework.urls')),
     # 原始json数据返回方法
     path("goods_source/", GoodsListViewSource.as_view(), name="goods_source"),
     # drf返回json数据方法
--- sample/MxShopProject/apps/goods/serializers.py
+++ sample/MxShopProject/apps/goods/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
+from apps.goods.models import Goods, GoodsCategory
 
 
-class GoodsSerializerDemo(serializers.Serializer):
+class CategorySerializerDemo(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
+
+
+# 通过ModelSerializer直接序列化Model数据
+class GoodsSerializerDemo(serializers.ModelSerializer):
     """
     商品数据序列化demo
     """
-    name = serializers.CharField(required=True, max_length=100)
-    click_num = serializers.IntegerField(default=0)
+    # name = serializers.CharField(required=True, max_length=100)
+    # click_num = serializers.IntegerField(default=0)
 
+    # 将goods中category外键显示出来
+    category = CategorySerializerDemo()
 
-
+    class Meta:
+        model = Goods
+        # 限定输出字段
+        # fields = ("name", "click_num", "add_time")
+        # 显示所有字段
+        fields = "__all__"
