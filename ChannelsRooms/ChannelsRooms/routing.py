0a5c574e9cf90c9763e2aa4b69f2aab7597dd2a9
freedom-wy <dazhuang_python@sina.com>
2022-03-24 16:40:08 +0800

    聊天室初始化-简单的websocket收发

--- ChannelsRooms/ChannelsRooms/routing.py
+++ ChannelsRooms/ChannelsRooms/routing.py
@@ -2,6 +2,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
+
+# 和django总的urls.py相同,用于寻找应用下的websocket路由
 application = ProtocolTypeRouter(
     {
         "websocket": AuthMiddlewareStack(
