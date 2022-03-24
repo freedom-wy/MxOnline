709440e10b03e2037ff5c51c30c7aacc66e44bf1
freedom-wy <dazhuang_python@sina.com>
2022-03-24 16:57:05 +0800

    聊天室-通过channels-redis实现群聊天

--- ChannelsRooms/chat/consumers.py
+++ ChannelsRooms/chat/consumers.py
@@ -1,21 +1,50 @@
 # chat/consumers.py
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        # 获取聊天室名称和组名
+        self.room_name = self.scope['url_route']['kwargs']['room_name']  # test1
+        # self.room_group_name = 'chat_%s' % self.room_name  # chat_test1
+        self.room_group_name = "chat_group"
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
+    # Receive message from WebSocket
     def receive(self, text_data=None, bytes_data=None):
-        # 接收从客户端发来的消息
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        # 拿到客户端消息后向客户端发回
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message
         }))
